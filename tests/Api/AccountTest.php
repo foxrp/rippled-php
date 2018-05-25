d1df458b6ae0d4d1a95f163181442b4a50198726
Mike Milano <coder1@gmail.com>
2018-05-25 04:48:40 -0700

    Added ApiTestCase for common API tests.

--- tests/Api/AccountTest.php
+++ tests/Api/AccountTest.php
@@ -1,80 +1,57 @@
 <?php
 
-namespace XRPhp\Tests\Api;
+namespace XRPHP\Tests\Api;
 
-use PHPUnit\Framework\TestCase;
-use XRPHP\Client;
-
-class AccountTest extends TestCase
+class AccountTest extends ApiTestCase
 {
-    /** @var Client */
-    private $client;
-
-    public function setUp()
+    public function __construct()
     {
-        $this->client = new \XRPHP\Client('https://s1.ripple.com:51234');
+        $this->testClass = \XRPHP\Api\Account::class;
+        parent::__construct();
     }
 
     public function testAccountChannels(): void
     {
-        $data = $this->client->account()->channels('rG1QQv2nh2gr7RCZ1P8YYcBUKCCN633jCn');
-
-        $this->assertTrue(is_array($data));
+        $this->runMethodTest('channels');
     }
 
     public function testAccountCurrencies(): void
     {
-        $data = $this->client->account()->currencies('rG1QQv2nh2gr7RCZ1P8YYcBUKCCN633jCn');
-
-        $this->assertTrue(is_array($data));
+        $this->runMethodTest('currencies');
     }
 
     public function testAccountInfo(): void
     {
-        $data = $this->client->account()->info('rG1QQv2nh2gr7RCZ1P8YYcBUKCCN633jCn');
-
-        $this->assertTrue(is_array($data));
+        $this->runMethodTest('info');
     }
 
     public function testAccountLines(): void
     {
-        $data = $this->client->account()->lines('rG1QQv2nh2gr7RCZ1P8YYcBUKCCN633jCn');
-
-        $this->assertTrue(is_array($data));
+        $this->runMethodTest('lines');
     }
 
     public function testAccountObjects(): void
     {
-        $data = $this->client->account()->objects('rG1QQv2nh2gr7RCZ1P8YYcBUKCCN633jCn');
-
-        $this->assertTrue(is_array($data));
+        $this->runMethodTest('objects');
     }
 
     public function testAccountOffers(): void
     {
-        $data = $this->client->account()->offers('rG1QQv2nh2gr7RCZ1P8YYcBUKCCN633jCn');
-
-        $this->assertTrue(is_array($data));
+        $this->runMethodTest('offers');
     }
 
     public function testAccountTx(): void
     {
-        $data = $this->client->account()->tx('rG1QQv2nh2gr7RCZ1P8YYcBUKCCN633jCn');
-
-        $this->assertTrue(is_array($data));
+        $this->runMethodTest('tx');
     }
 
     public function testAccountGatewayBalances(): void
     {
-        $data = $this->client->account()->gatewayBalances('rG1QQv2nh2gr7RCZ1P8YYcBUKCCN633jCn');
-
-        $this->assertTrue(is_array($data));
+        $this->runMethodTest('gatewayBalances');
     }
 
     public function testAccountNorippleCheck(): void
     {
-        $data = $this->client->account()->norippleCheck('rG1QQv2nh2gr7RCZ1P8YYcBUKCCN633jCn');
-
-        $this->assertTrue(is_array($data));
+        $this->runMethodTest('norippleCheck');
     }
 }
