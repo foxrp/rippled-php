91346b3de16231947ed6f6f6baef8c14b417ce52
Mike Milano <coder1@gmail.com>
2018-05-30 03:36:14 -0700

    Added account_offers method.

---
+++ src/Api/Account/AccountOffersMethod.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace XRPHP\Api\Account;
+
+use XRPHP\Api\Method;
+
+/**
+ * The account_offers method retrieves a list of offers made by a given account that are outstanding as of a particular
+ * ledger version.
+ *
+ * @link https://developers.ripple.com/account_offers.html Documentation of account_offers.
+ * @package XRPHP\Api\Account
+ */
+class AccountOffersMethod extends Method
+{
+    public function getValidParameters(): array
+    {
+        return [
+            'account',
+            'ledger_hash',
+            'ledger_index',
+            'limit',
+            'marker'
+        ];
+    }
+
+    /**
+     * Validates parameters.
+     *
+     * @param array|null $params
+     * @throws \BadMethodCallException
+     */
+    public function validateParameters(array $params = null): void
+    {
+        if (!isset($params['account'])) {
+            throw new \BadMethodCallException('Missing parameter: account');
+        }
+    }
+}
