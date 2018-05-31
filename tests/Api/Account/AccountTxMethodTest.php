755d738d802ca33218b60b2a2c06f1947eeaa7c1
Mike Milano <coder1@gmail.com>
2018-05-31 04:16:58 -0700

    Added account_tx tests.

---
+++ tests/Api/Account/AccountTxMethodTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace XRPHP\Tests\Api\Account;
+
+use XRPHP\Tests\Api\MethodTestCase;
+
+class AccountTxMethodTest extends MethodTestCase
+{
+    public function testSuccessMinParameters(): void
+    {
+        // Setup a successful response.
+        $this->setResponse($this->getJsonFromFile('account_tx_success'));
+
+        $params = ['account' => 'rG1QQv2nh2gr7RCZ1P8YYcBUKCCN633jCn', 'ledger_index' => 'current'];
+        $method = $this->client->method('account_tx', $params);
+
+        $this->assertEquals('account_tx', $method->getMethod());
+        $this->assertSame($params, $method->getParams());
+
+        $res = $method->execute();
+
+        $this->assertTrue($res->isSuccess(), 'isSuccess is not true');
+        $this->assertTrue($res->isValidated(), 'isValidated is not null');
+    }
+
+    public function testMissingAccountWithEmptyArrayThrowsException()
+    {
+        $this->expectException(\BadMethodCallException::class);
+        $this->client->method('account_tx', []);
+    }
+
+    public function testMissingAccountWithNullThrowsException()
+    {
+        $this->expectException(\BadMethodCallException::class);
+        $this->client->method('account_tx', null);
+    }
+
+    public function testInvalidParamsThrowsException()
+    {
+        $this->expectException(\BadMethodCallException::class);
+        $this->client->method('account_tx', [
+            'account' => 'rG1QQv2nh2gr7RCZ1P8YYcBUKCCN633jCn',
+            'ledger_index' => 'current',
+            'not_a_param' => 'should cause an exception'
+        ]);
+    }
+}
