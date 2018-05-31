e8a9247f7752833ba3a847d0ab8308af8f39c1b0
Mike Milano <coder1@gmail.com>
2018-05-31 04:17:30 -0700

    Added gateway_balances tests.

---
+++ tests/Api/Account/GatewayBalancesMethodTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace XRPHP\Tests\Api\Account;
+
+use XRPHP\Tests\Api\MethodTestCase;
+
+class GatewayBalancesMethodTest extends MethodTestCase
+{
+    public function testSuccessMinParameters(): void
+    {
+        // Setup a successful response.
+        $this->setResponse($this->getJsonFromFile('gateway_balances_success'));
+
+        $params = ['account' => 'rG1QQv2nh2gr7RCZ1P8YYcBUKCCN633jCn'];
+        $method = $this->client->method('gateway_balances', $params);
+
+        $this->assertEquals('gateway_balances', $method->getMethod());
+        $this->assertSame($params, $method->getParams());
+
+        $res = $method->execute();
+
+        $this->assertTrue($res->isSuccess(), 'isSuccess is not true');
+        $this->assertTrue($res->isValidated(), 'isValidated is not null');
+    }
+
+    public function testMissingAccountWithEmptyArrayThrowsException()
+    {
+        $this->expectException(\BadMethodCallException::class);
+        $this->client->method('gateway_balances', []);
+    }
+
+    public function testMissingAccountWithNullThrowsException()
+    {
+        $this->expectException(\BadMethodCallException::class);
+        $this->client->method('gateway_balances', null);
+    }
+
+    public function testInvalidParamsThrowsException()
+    {
+        $this->expectException(\BadMethodCallException::class);
+        $this->client->method('gateway_balances', [
+            'account' => 'rG1QQv2nh2gr7RCZ1P8YYcBUKCCN633jCn',
+            'not_a_param' => 'should cause an exception'
+        ]);
+    }
+}
