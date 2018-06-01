0d986a9709e049d8168414576b84b687b12cb3da
Mike Milano <coder1@gmail.com>
2018-06-01 04:10:28 -0700

    Added tests for ledger_entry.

--- tests/Api/Anon/Ledger/LedgerEntryMethodTest.php
+++ tests/Api/Anon/Ledger/LedgerEntryMethodTest.php
@@ -2,6 +2,7 @@
 
 namespace XRPHP\Tests\Api\Anon\Account;
 
+use XRPHP\Exception\InvalidParameterException;
 use XRPHP\Tests\Api\MethodTestCase;
 
 class LedgerEntryMethodTest extends MethodTestCase
@@ -20,5 +21,57 @@ class LedgerEntryMethodTest extends MethodTestCase
         $res = $method->execute();
 
         $this->assertTrue($res->isSuccess(), 'isSuccess is not true');
+    }
+
+    public function testDirectoryNoDirectoryRootOrOwnerException()
+    {
+        $this->expectException(InvalidParameterException::class);
+        $this->expectExceptionMessageRegExp('/directory\.owner or directory\.dir_root/');
+
+        $this->client->method('ledger_entry', ['directory' => 'foo']);
+    }
+
+    public function testOfferNoOfferAccountException()
+    {
+        $this->expectException(InvalidParameterException::class);
+        $this->expectExceptionMessageRegExp('/offer\.account/');
+
+        $this->client->method('ledger_entry', [
+            'offer' => 'foo',
+            'offer.seq' => 'bar'
+        ]);
+    }
+
+    public function testOfferNoOfferSeqException()
+    {
+        $this->expectException(InvalidParameterException::class);
+        $this->expectExceptionMessageRegExp('/offer\.seq/');
+
+        $this->client->method('ledger_entry', [
+            'offer' => 'foo',
+            'offer.account' => 'bar'
+        ]);
+    }
+
+    public function testRippleStateNoAccountsException()
+    {
+        $this->expectException(InvalidParameterException::class);
+        $this->expectExceptionMessageRegExp('/ripple_state\.accounts/');
+
+        $this->client->method('ledger_entry', [
+            'ripple_state' => 'foo',
+            'ripple_state.currency' => 'bar'
+        ]);
+    }
+
+    public function testRippleStateNoCurrencyException()
+    {
+        $this->expectException(InvalidParameterException::class);
+        $this->expectExceptionMessageRegExp('/ripple_state\.currency/');
+
+        $this->client->method('ledger_entry', [
+            'ripple_state' => 'foo',
+            'ripple_state.accounts' => 'bar'
+        ]);
     }
 }
