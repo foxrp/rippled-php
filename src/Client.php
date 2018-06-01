17e28d7143de220530fff3eeb2b3a3a37bf0408b
Mike Milano <coder1@gmail.com>
2018-06-01 03:09:57 -0700

    Added ledger_data method.

--- src/Client.php
+++ src/Client.php
@@ -51,7 +51,7 @@ class Client
             'ledger' => \XRPHP\Api\Anon\Ledger\LedgerMethod::class,
             'ledger_closed' => \XRPHP\Api\Anon\Ledger\LedgerClosedMethod::class,
             'ledger_current' => \XRPHP\Api\Anon\Ledger\LedgerCurrentMethod::class,
-            'ledger_data' => \XRPHP\Api\Anon\Account\NorippleCheckMethod::class,
+            'ledger_data' => \XRPHP\Api\Anon\Ledger\LedgerDataMethod::class,
             'ledger_entry' => \XRPHP\Api\Anon\Account\NorippleCheckMethod::class,
         ];
     }
---
+++ tests/Api/Anon/Ledger/LedgerDataMethodTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace XRPHP\Tests\Api\Anon\Account;
+
+use XRPHP\Tests\Api\MethodTestCase;
+
+class LedgerDataMethodTest extends MethodTestCase
+{
+    public function testSuccessMinParameters(): void
+    {
+        // Setup a successful response.
+        $this->setResponse($this->getJsonFromFile('ledger_data_success'));
+
+        $params = [];
+        $method = $this->client->method('ledger_data', $params);
+
+        $this->assertEquals('ledger_data', $method->getMethod());
+        $this->assertSame($params, $method->getParams());
+
+        $res = $method->execute();
+
+        $this->assertTrue($res->isSuccess(), 'isSuccess is not true');
+    }
+
+    public function testInvalidParamsThrowsException()
+    {
+        $this->expectException(\BadMethodCallException::class);
+        $this->client->method('ledger_data', [
+            'not_a_param' => 'should cause an exception'
+        ]);
+    }
+}
