6adb11963d9f1e9d11c2d3e56641f1a2e8a157a5
Mike Milano <coder1@gmail.com>
2018-06-05 02:38:27 -0700

    Added submit method tests.

---
+++ tests/Api/Anon/Transaction/SubmitMethodTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace XRPHP\Tests\Api\Anon\Transaction;
+
+use XRPHP\Exception\InvalidParameterException;
+use XRPHP\Tests\Api\MethodTestCase;
+
+class SubmitMethodTest extends MethodTestCase
+{
+    public function __construct()
+    {
+        $this->method = 'submit';
+        parent::__construct();
+    }
+
+    public function testSuccessMinParameters(): void
+    {
+        // Setup a successful response.
+        $this->setResponse($this->getJsonFromFile('submit_success'));
+
+        $params = ['tx_blob' => 'test'];
+        $method = $this->client->method('submit', $params);
+
+        $this->assertEquals('submit', $method->getMethod());
+        $this->assertSame($params, $method->getParams());
+
+        $res = $method->execute();
+
+        $this->assertTrue($res->isSuccess(), 'isSuccess is not true');
+    }
+
+    public function testMissingTxBlobWithNullThrowsException()
+    {
+        $this->expectException(InvalidParameterException::class);
+        $this->expectExceptionMessageRegExp('/tx_blob/');
+        $this->client->method('submit', null);
+    }
+}
