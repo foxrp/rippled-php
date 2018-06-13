484a69e030dfb639eb23a237791914fcdf9a13f2
Mike Milano <coder1@gmail.com>
2018-06-13 03:42:16 -0700

    Test updates.

--- tests/TransactionTest.php
+++ tests/TransactionTest.php
@@ -4,21 +4,22 @@ namespace XRPHP\Tests;
 
 use PHPUnit\Framework\TestCase;
 use XRPHP\Client;
+use XRPHP\Exception\TransactionSignException;
 use XRPHP\Transaction;
-use XRPHP\XRPHP;
 
 /**
 *  Test for Client class
 */
-class XRPHPTest extends TestCase
+class TransactionTest extends TestCase
 {
     /**
     * Check for syntax errors
     */
     public function testIsThereAnySyntaxError(): void
     {
-        $xrphp = new XRPHP('https://example.com');
-        $this->assertInternalType('object', $xrphp);
+        $tx = $this->getTx();
+        $transaction = new Transaction($tx);
+        $this->assertInternalType('object', $transaction);
     }
 
     /**
@@ -26,8 +27,9 @@ class XRPHPTest extends TestCase
      */
     public function testNoClientIsCreatedWithConstructor(): void
     {
-        $xrphp = new XRPHP();
-        $client = $xrphp->getClient();
+        $tx = $this->getTx();
+        $transaction = new Transaction($tx);
+        $client = $transaction->getClient();
         $this->assertNull($client);
     }
 
@@ -36,57 +38,19 @@ class XRPHPTest extends TestCase
      */
     public function testClientIsCreatedWithConstructor(): void
     {
-        $xrphp = new XRPHP('https://example.com');
-        $client = $xrphp->getClient();
+        $tx = $this->getTx();
+        $client = new Client('https://example.com');
+        $transaction = new Transaction($tx, $client);
+        $client = $transaction->getClient();
 
         $this->assertNotNull($client);
         $this->assertEquals(\XRPHP\Client::class, \get_class($client));
     }
 
-    public function testTransactionFactory()
-    {
-        $xrphp = new XRPHP('https://example.com');
-        $txJson = $this->getTx();
-
-        $transaction = $xrphp->transaction($txJson);
-        $this->assertEquals(\XRPHP\Transaction::class, \get_class($transaction));
-
-        $client = $transaction->getClient();
-        $this->assertEquals(\XRPHP\Client::class, \get_class($client));
-    }
-
     /*
-    public function testGetTx()
+    public function testSignLocal()
     {
-        $xrp = new Client('https://s.altnet.rippletest.net:51234');
-
-        $params = [
-            'tx_json' => [
-                'TransactionType' => 'Payment',
-                'Account' => 'rQBnNY5w5cALHbMaue2VefSzuBfxafwqp9',
-                'Destination' => 'rnQ1WgToG2RL9Fjmofif9ixYVgJTi6BLas',
-                'Amount' => '1000000'
-            ],
-            'secret' => 'saEiBcexrULNPiPn5MD3GPJeiU55U'
-        ];
-        $method = $xrp->method('submit', $params);
-        $res = $method->execute();
-
-        $params = [
-            'account' => 'rQBnNY5w5cALHbMaue2VefSzuBfxafwqp9'
-        ];
-        $method = $xrp->method('account_info', $params);
-        $res = $method->execute();
-
-        print_r($res->getResult());
-
-
-    }
-    */
-
-    public function testSign()
-    {
-        $client = new Client('https://s.altnet.rippletest.net:51234');
+        $client = new Client('https://example.com');
         $params = [
             'TransactionType' => 'Payment',
             'Account' => 'rQBnNY5w5cALHbMaue2VefSzuBfxafwqp9',
@@ -96,11 +60,7 @@ class XRPHPTest extends TestCase
         ];
 
         $tx = new Transaction($params, $client);
-        $tx->sign('saEiBcexrULNPiPn5MD3GPJeiU55U');
-
-        print_r($tx->getTx());
-
-        /*
+        $tx->signLocal('saEiBcexrULNPiPn5MD3GPJeiU55U');
 
         $xrp = new Client('https://s.altnet.rippletest.net:51234');
 
@@ -108,8 +68,9 @@ class XRPHPTest extends TestCase
         $res = $method->execute();
 
         print_r($res);
-        */
+
     }
+         */
 
     /*
     public function testGetAccountInfo()
@@ -124,14 +85,16 @@ class XRPHPTest extends TestCase
 
         print_r($res->getResult());
     }
-*/
+    */
+
     private function getTx()
     {
         return [
             'TransactionType' => 'Payment',
             'Account' => 'rQBnNY5w5cALHbMaue2VefSzuBfxafwqp9',
             'Destination' => 'rnQ1WgToG2RL9Fjmofif9ixYVgJTi6BLas',
-            'Amount' => '1000000'
+            'Amount' => '1000000',
+            'Fee' => '000012'
         ];
     }
 
