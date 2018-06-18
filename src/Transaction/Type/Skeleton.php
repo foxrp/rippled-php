14430aefa971bd615a55b8d4289c216264dd7dd4
Mike Milano <coder1@gmail.com>
2018-06-18 05:10:54 -0700

    Addd Skeleton class for generator to copy from.

---
+++ src/Transaction/Type/Skeleton.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace XRPHP\Transaction\Type;
+
+use XRPHP\Transaction\TypeField;
+
+/**
+ * @link %reference% Skeleton transaction type documentation.
+ * @package XRPHP\TransactionType
+ */
+class Skeleton extends AbstractType
+{
+    /**
+     * Skeleton constructor.
+     * @param array|null $params
+     * @throws \XRPHP\Exception\InvalidParameterException
+     * @throws \XRPHP\Exception\TransactionTypeFieldException
+     */
+    public function __construct(array $params = null)
+    {
+        // GENERATED CODE FROM bin/generate.php types
+        // BEGIN GENERATED
+        // END GENERATED
+
+        parent::__construct($params);
+    }
+}
