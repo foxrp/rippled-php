24cd687cb098115c0a64da7f49ed60bbd15f8c34
Mike Milano <coder1@gmail.com>
2018-06-18 05:10:17 -0700

    Added generator command.

---
+++ bin/generate.php
@@ -0,0 +1,13 @@
+#!/usr/bin/env php
+<?php
+// application.php
+
+require __DIR__.'/../vendor/autoload.php';
+
+use Symfony\Component\Console\Application;
+
+$application = new Application();
+
+$application->add(new \XRPHP\Command\GenerateTypesCommand());
+
+$application->run();
