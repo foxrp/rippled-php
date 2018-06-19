8ad9383d013f1542f40ed7c436968658897537a6
Mike Milano <coder1@gmail.com>
2018-06-19 06:48:32 -0700

    Updated Skel test.

--- tests/Transaction/Type/SkeletonTest.php
+++ tests/Transaction/Type/SkeletonTest.php
@@ -4,8 +4,6 @@ namespace XRPHP\Tests\Transaction\Type;
 
 use PHPUnit\Framework\TestCase;
 use XRPHP\Transaction\Type\Skeleton;
-use XRPHP\Transaction\TypeField;
-use XRPHP\Transaction\TypeInterface;
 
 class SkeletonTest extends TestCase
 {
