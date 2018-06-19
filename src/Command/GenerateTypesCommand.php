1a419f8f29e8c9414a23f47804eb8df7ccaa2bf3
Mike Milano <coder1@gmail.com>
2018-06-18 21:50:54 -0700

    Added generator command.

--- src/Command/GenerateTypesCommand.php
+++ src/Command/GenerateTypesCommand.php
@@ -5,6 +5,7 @@ namespace XRPHP\Command;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Filesystem\Filesystem;
 
 class GenerateTypesCommand extends Command
 {
@@ -23,12 +24,74 @@ class GenerateTypesCommand extends Command
     {
         $this->data = json_decode(file_get_contents(__DIR__.'/../../rippled-spec/transactions.json'), true);
         $this->updateAbstract();
+        $this->updateTypes();
+    }
+
+    private function updateTypes(): void
+    {
+        $dir = __DIR__.'/../Transaction/Type/';
+        $skel = $dir . 'Skeleton.php';
+        $fs = new Filesystem();
+
+        foreach ($this->data['types'] as $type) {
+
+            $file = $dir . $type['name'] .'.php';
+
+            // Create class file if it doesn't exist.
+            if (!$fs->exists($file)) {
+
+                $content = file_get_contents($skel);
+
+                // Replace Skeleton class strings with values from this transaction type.
+                $search = ['Skeleton'];
+                $replace = [$type['name']];
+                $newContent = str_replace($search, $replace, $content);
+
+                // Write the file.
+                file_put_contents($file, $newContent, LOCK_EX);
+            }
+
+            // Update headers.
+            $this->replaceClassComment($type, $file);
+
+            // Update fields.
+            $this->generateFields($type['fields'], $file);
+        }
     }
 
     private function updateAbstract(): void
     {
+        $this->generateFields($this->data['common_fields']['fields'], __DIR__.'/../Transaction/Type/AbstractType.php');
+    }
+
+    private function replaceClassComment(array $type, string $file): void
+    {
+        $desc = wordwrap($type['description'], 117);
+        $lines = explode("\n", $desc);
+        $desc = '';
+        foreach ($lines as $line) {
+            $desc .= "\n * " . $line;
+        }
+
+        $content = file_get_contents($file);
+        $search = "/\/\*\*.*?\s\*\//s";
+        $replace = <<<EOF
+/**
+ * {$type['name']} Transaction Type Class
+ *{$desc}
+ *
+ * {$type['reference']} {$type['name']} transaction type documentation.
+ */
+EOF;
+
+        $newContent = preg_replace($search, $replace, $content, 1);
+        file_put_contents($file, $newContent);
+    }
+
+    private function generateFields(array $fields, string $file)
+    {
         $code = '';
-        foreach ($this->data['common_fields']['fields'] as $field) {
+        foreach ($fields as $field) {
             $type = $field['name'];
             $req = isset($field['required']) && $field['required'] === true ? 'true' : 'false';
             $af = isset($field['auto_fillable']) && $field['auto_fillable'] === true ? 'true' : 'false';
@@ -41,11 +104,10 @@ class GenerateTypesCommand extends Command
 
 ';
         }
-
-        $this->replaceCode(__DIR__.'/../Transaction/Type/AbstractType.php', $code);
+        $this->replaceCode($code, $file);
     }
 
-    private function replaceCode($file, $code): void
+    private function replaceCode($code, $file): void
     {
         $content = file_get_contents($file);
         $search = "/\/\/\sBEGIN\sGENERATED(.*)END\sGENERATED/s";
