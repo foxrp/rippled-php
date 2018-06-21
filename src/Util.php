1b5fe45967bbcb8a823549b58c7c22adb517305d
Mike Milano <coder1@gmail.com>
2018-06-20 17:47:20 -0700

    Added util class for static helpers.

---
+++ src/Util.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace XRPHP;
+
+class Util
+{
+    /**
+     * Converts snake case to pascal case.
+     *
+     * @param string $key Snake case key
+     * @return string Pascal case key
+     */
+    public static function CaseFromSnake(string $key): string
+    {
+        return str_replace('_', '', ucwords($key, '_'));
+    }
+
+}
