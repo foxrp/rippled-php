7e39d6a29ac3f770fc29acd482e14727941ebd57
Mike Milano <coder1@gmail.com>
2018-06-22 02:42:15 -0700

    Removed un-used interface.

--- src/Api/MethodInterface.php
+++
@@ -1,9 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace XRPHP\Api;
-
-interface MethodInterface
-{
-    public function getValidParameters(): array;
-    public function validateParameters(array $params): void;
-}
